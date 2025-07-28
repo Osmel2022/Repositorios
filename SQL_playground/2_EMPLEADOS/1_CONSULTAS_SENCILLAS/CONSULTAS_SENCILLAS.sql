af2d11441b3bffb56dfc94fcce1f9ed08b5148c1
Osmel Torres <osmel86mtz@gmail.com>
2025-07-28 23:52:42 +0200

    Add SQL queries for employee data retrieval

---
+++ SQL_playground/2_EMPLEADOS/1_CONSULTAS_SENCILLAS/CONSULTAS_SENCILLAS.sql
@@ -0,0 +1,13 @@
+-- 1_Lista el primer apellido de todos los empleados.
+
+-- 2_Lista el primer apellido de los empleados eliminando los apellidos que estén repetidos.
+select distinct apellido1 from empleado
+
+-- 3_Lista todas las columnas de la tabla empleado.
+
+-- 4_Lista el nombre y los apellidos de todos los empleados.
+
+-- 5_Lista el identificador de los departamentos de los empleados que aparecen en la tabla empleado.
+
+-- 6_Lista el identificador de los departamentos de los empleados que aparecen en la tabla empleado, eliminando los identificadores que aparecen repetidos.
+select distinct id_departamento from empleado
