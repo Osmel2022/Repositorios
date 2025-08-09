830c9c003c8095fa1d3fa348c6349675621c28c4
Osmel Torres <osmel86mtz@gmail.com>
2025-08-09 23:59:50 +0200

    Add SQL queries to count employees per department and include departments with no employees

--- SQL_playground/2_EMPLEADOS/4_CONSULTAS_RESUMEN/CONSULTAS_RESUMEN.sql
+++ SQL_playground/2_EMPLEADOS/4_CONSULTAS_RESUMEN/CONSULTAS_RESUMEN.sql
@@ -31,3 +31,24 @@ SELECT COUNT(*)  FROM empleados;
 select count(*) from    empleado
     where apellido2 is not null
 
+-- 9_Calcula el número de empleados que hay en cada departamento. Tienes que devolver dos columnas, 
+    --una con el nombre del departamento y otra con el número de empleados que tiene asignados.
+--MY_QUERY
+select id_departamento as id_d, count(*) from empleado
+    where id_departamento is not null
+    group by id_d
+
+-- 10_Calcula el nombre de los departamentos que tienen más de 2 empleados. El resultado debe tener dos columnas, 
+    --una con el nombre del departamento y otra con el número de empleados que tiene asignados.
+--MY_QUERY
+select d.nombre ,  count(e.id_departamento)  from empleado e
+    join departamento d on e.id_departamento = d.id
+    group by d.nombre
+    having count(*)>2
+
+-- 11_Calcula el número de empleados que trabajan en cada uno de los departamentos.
+    -- El resultado de esta consulta también tiene que incluir aquellos departamentos que no tienen ningún empleado asociado.
+SELECT d.nombre, COUNT(e.id) AS numero_empleados    
+    FROM departamentos d
+    LEFT JOIN empleados e ON d.id = e.id_departamento
+    GROUP BY d.nombre;
