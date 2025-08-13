66b42c12dbf025fcf31eee0a44024a7b07c8d736
Osmel Torres <osmel86mtz@gmail.com>
2025-08-13 23:53:02 +0200

    Add SQL queries to retrieve all orders per customer and orders involving sales representatives

--- SQL_playground/3_VENTAS/2_COMPOSICION_INTERNA/COMPOSICION_INTERNA.sql
+++ SQL_playground/3_VENTAS/2_COMPOSICION_INTERNA/COMPOSICION_INTERNA.sql
@@ -5,9 +5,17 @@ from cliente
     where id in (select distinct id_cliente from pedido)
     order by nombre, apellido1, apellido2
     
--- 2_
+-- 2_Devuelve un listado que muestre todos los pedidos que ha realizado cada cliente. El resultado debe mostrar todos los datos de los pedidos y del cliente.
+    -- El listado debe mostrar los datos de los clientes ordenados alfabéticamente.
+select * from pedido p   
+    inner join cliente c on p.id_cliente = c.id
+    order by c.nombre, c.apellido1, c.apellido2
 
--- 3_
+-- 3_Devuelve un listado que muestre todos los pedidos en los que ha participado un comercial. El resultado debe mostrar todos los datos de los pedidos y de los comerciales.
+    -- El listado debe mostrar los datos de los comerciales ordenados alfabéticamente.
+select * from pedido p
+    inner join comercial c on p.id_comercial = c.id 
+    order by c.nombre, c.apellido1, c.apellido2
 
 -- 4_
 
