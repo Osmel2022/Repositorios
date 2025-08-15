93f5ea32d1cf49a0b37b873927ffaa7692334267
Osmel Torres <osmel86mtz@gmail.com>
2025-08-15 09:39:33 +0200

    Enhance SQL script with additional queries for customer orders and commercial participation

--- SQL_playground/3_VENTAS/2_COMPOSICION_INTERNA/COMPOSICION_INTERNA.sql
+++ SQL_playground/3_VENTAS/2_COMPOSICION_INTERNA/COMPOSICION_INTERNA.sql
@@ -17,10 +17,29 @@ select * from pedido p
     inner join comercial c on p.id_comercial = c.id 
     order by c.nombre, c.apellido1, c.apellido2
 
--- 4_
+-- 4_Devuelve un listado que muestre todos los clientes, 
+    --con todos los pedidos que han realizado y con los datos de los comerciales asociados a cada pedido.
+select * from cliente c
+      join pedido p on c.id = p.id_cliente
+      join comercial co on p.id_comercial = co.id
 
--- 5_
+-- 5_Devuelve un listado de todos los clientes que realizaron un pedido durante el año 2017,    
+    --cuya cantidad esté entre 300 € y 1000 €.
+select distinct * from cliente c
+    inner join pedido p on c.id = p.id_cliente
+    where p.fecha >= '2017-01-01' and p.fecha < '2018-01-01'
+    and p.total >= 300 and p.total <= 1000
 
--- 6_
+-- 6_Devuelve el nombre y los apellidos de todos los comerciales que ha participado en algún pedido realizado por María Santana Moreno.
+select distinct  c.apellido1, c.apellido2,c.nombre
+from comercial c
+    inner join pedido p on c.id = p.id_comercial
+    inner join cliente cl on p.id_cliente = cl.id
+where   cl.nombre = 'María' and cl.apellido1 = 'Santana' and cl.apellido2 = 'Moreno' 
 
--- 7_
+-- 7_Devuelve el nombre de todos los clientes que han realizado algún pedido con el comercial Daniel Sáez Vega.
+select distinct cl.apellido1,cl.apellido2,cl.nombre
+from cliente cl
+    inner join pedido p on cl.id = p.id_cliente
+    inner join comercial c on p.id_comercial = c.id
+where c.nombre = 'Daniel' and c.apellido1 = 'Sáez' and c.apellido2 = 'Vega' 
