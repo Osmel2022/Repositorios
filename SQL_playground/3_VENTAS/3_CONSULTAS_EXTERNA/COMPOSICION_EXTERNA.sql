8890fc23fb95d5292949e89acb257ae428f6298e
Osmel Torres <osmel86mtz@gmail.com>
2025-08-16 23:48:51 +0200

    Add SQL comments for customer and commercial order listings

---
+++ SQL_playground/3_VENTAS/3_CONSULTAS_EXTERNA/COMPOSICION_EXTERNA.sql
@@ -0,0 +1,13 @@
+-- 1_Devuelve un listado con todos los clientes junto con los datos de los pedidos que han realizado. 
+    --Este listado también debe incluir los clientes que no han realizado ningún pedido.
+    -- El listado debe estar ordenado alfabéticamente por el primer apellido, segundo apellido y nombre de los clientes.
+
+-- 2_Devuelve un listado con todos los comerciales junto con los datos de los pedidos que han realizado.
+    -- Este listado también debe incluir los comerciales que no han realizado ningún pedido. 
+    -- El listado debe estar ordenado alfabéticamente por el primer apellido, segundo apellido y nombre de los comerciales.
+
+-- 3_
+
+-- 4_
+
+-- 5_
