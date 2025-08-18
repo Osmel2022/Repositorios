be5f089d7c9a8be1e0bccb69b8daaee66a34dd30
Osmel Torres <osmel86mtz@gmail.com>
2025-08-18 23:58:01 +0200

    Reintroduce SQL script for client and commercial order listings with detailed comments

--- SQL_playground/3_VENTAS/3_COMPOSICION_EXTERNA/COMPOSICION_EXTERNA.sql
+++ SQL_playground/3_VENTAS/3_COMPOSICION_EXTERNA/COMPOSICION_EXTERNA.sql
@@ -1,6 +1,9 @@
 -- 1_Devuelve un listado con todos los clientes junto con los datos de los pedidos que han realizado. 
     --Este listado también debe incluir los clientes que no han realizado ningún pedido.
     -- El listado debe estar ordenado alfabéticamente por el primer apellido, segundo apellido y nombre de los clientes.
+select apellido1,apellido2,nombre,p.* from cliente c
+    left join pedido p on c.id = p.id_cliente
+    order by apellido1,apellido2,nombre
 
 -- 2_Devuelve un listado con todos los comerciales junto con los datos de los pedidos que han realizado.
     -- Este listado también debe incluir los comerciales que no han realizado ningún pedido. 
