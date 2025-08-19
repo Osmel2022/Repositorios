4a4cb15d4cb61adb55c312f485db79f7b75cdfdb
Osmel Torres <osmel86mtz@gmail.com>
2025-08-19 23:36:43 +0200

    Add SQL query to list commercial orders with sorting by last names

--- SQL_playground/3_VENTAS/3_COMPOSICION_EXTERNA/COMPOSICION_EXTERNA.sql
+++ SQL_playground/3_VENTAS/3_COMPOSICION_EXTERNA/COMPOSICION_EXTERNA.sql
@@ -8,6 +8,9 @@ select apellido1,apellido2,nombre,p.* from cliente c
 -- 2_Devuelve un listado con todos los comerciales junto con los datos de los pedidos que han realizado.
     -- Este listado también debe incluir los comerciales que no han realizado ningún pedido. 
     -- El listado debe estar ordenado alfabéticamente por el primer apellido, segundo apellido y nombre de los comerciales.
+select * from comercial c
+    left join pedido p on c.id =p.id_comercial
+    order by apellido1,apellido2,nombre
 
 -- 3_Devuelve un listado que solamente muestre los clientes que no han realizado ningún pedido.
 
