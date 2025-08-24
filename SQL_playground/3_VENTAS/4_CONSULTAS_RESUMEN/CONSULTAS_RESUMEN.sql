32b183141166de63e08627eecd096138497c48cd
Osmel Torres <osmel86mtz@gmail.com>
2025-08-24 21:10:14 +0200

    Add SQL queries to calculate total and average order amounts

---
+++ SQL_playground/3_VENTAS/4_CONSULTAS_RESUMEN/CONSULTAS_RESUMEN.sql
@@ -0,0 +1,19 @@
+-- 1_Calcula la cantidad total que suman todos los pedidos que aparecen en la tabla pedido.
+select sum(total) from pedido
+
+-- 2_Calcula la cantidad media de todos los pedidos que aparecen en la tabla pedido.
+select avg(total) from pedido
+
+-- 3_
+
+-- 4_
+
+-- 5_
+
+-- 6_
+
+-- 7_
+
+-- 8_
+
+-- 9_
