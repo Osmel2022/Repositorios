d590bfd5ea82c42c157a321057ba5407976fa093
Osmel Torres <osmel86mtz@gmail.com>
2025-08-24 21:41:08 +0200

    Add SQL queries to count distinct commercials and clients, and find maximum order total

--- SQL_playground/3_VENTAS/4_CONSULTAS_RESUMEN/CONSULTAS_RESUMEN.sql
+++ SQL_playground/3_VENTAS/4_CONSULTAS_RESUMEN/CONSULTAS_RESUMEN.sql
@@ -4,11 +4,14 @@ select sum(total) from pedido
 -- 2_Calcula la cantidad media de todos los pedidos que aparecen en la tabla pedido.
 select avg(total) from pedido
 
--- 3_
+-- 3_Calcula el número total de comerciales distintos que aparecen en la tabla pedido.
+select  count( distinct id_comercial) from pedido
 
--- 4_
+-- 4_Calcula el número total de clientes que aparecen en la tabla cliente.
+select count(id) from cliente
 
--- 5_
+-- 5_Calcula cuál es la mayor cantidad que aparece en la tabla pedido.
+select max(total) from pedido
 
 -- 6_
 
