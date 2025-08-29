0a7b5f47972670821f8fb7f4be5f597a5e8683fb
Osmel Torres <osmel86mtz@gmail.com>
2025-08-29 23:58:05 +0200

    Add query to display maximum order value per client, including clients with no orders

--- SQL_playground/3_VENTAS/4_CONSULTAS_RESUMEN/CONSULTAS_RESUMEN.sql
+++ SQL_playground/3_VENTAS/4_CONSULTAS_RESUMEN/CONSULTAS_RESUMEN.sql
@@ -67,7 +67,9 @@ select c.id,c.nombre,c.apellido1,c.apellido2,count(p.id) from cliente c
 
 -- 13_Devuelve un listado que muestre el identificador de cliente, nombre, primer apellido y el valor de la máxima cantidad del pedido realizado por cada uno de los clientes
     -- El resultado debe mostrar aquellos clientes que no han realizado ningún pedido indicando que la máxima cantidad de sus pedidos realizados es 0. Puede hacer uso de la función IFNULL.
-
+select c.id,c.nombre,c.apellido1,c.apellido2,ifnull(max(p.total),0 )from cliente c 
+    left join pedido p on c.id =p.id_cliente
+    group by c.id,c.nombre,c.apellido1,c.apellido2
 -- 14_
 
 -- 15_
