ce93522d1a0ca247800a4350475d9fe26ea14dec
Osmel Torres <osmel86mtz@gmail.com>
2025-09-08 23:23:31 +0200

    Add queries to list office details and employees under a specific manager in CONSULTAS_SENCILLAS.sql

--- SQL_playground/4_JARDINERIA/1_CONSULTAS_SENCILLAS/CONSULTAS_SENCILLAS.sql
+++ SQL_playground/4_JARDINERIA/1_CONSULTAS_SENCILLAS/CONSULTAS_SENCILLAS.sql
@@ -1,9 +1,13 @@
 -- 1_Devuelve un listado con el código de oficina y la ciudad donde hay oficinas.
 select codigo_oficina, ciudad from oficina;
 
--- 2_
+-- 2_Devuelve un listado con la ciudad y el teléfono de las oficinas de España.
+select ciudad, telefono from oficina
+    where pais = 'España';
 
--- 3_
+-- 3_Devuelve un listado con el nombre, apellidos y email de los empleados cuyo jefe tiene un código de jefe igual a 7.
+select nombre, apellido1, apellido2, email,codigo_jefe from empleado
+    where codigo_jefe = 7;
 
 -- 4_
 
