63e267bc3ea839b605b81c70915282e00651f129
Osmel Torres <osmel86mtz@gmail.com>
2025-09-11 15:24:14 +0200

    Add queries to retrieve orders delivered late, rejected orders in 2009, and orders delivered in January

--- SQL_playground/4_JARDINERIA/1_CONSULTAS_SENCILLAS/CONSULTAS_SENCILLAS.sql
+++ SQL_playground/4_JARDINERIA/1_CONSULTAS_SENCILLAS/CONSULTAS_SENCILLAS.sql
@@ -43,3 +43,28 @@ select distinct estado from pedido;
     --fecha esperada y fecha de entrega de los pedidos que no han sido entregados a tiempo.
 select codigo_pedido, codigo_cliente, fecha_esperada, fecha_entrega from pedido
     where fecha_entrega > fecha_esperada;
+
+-- 10_Devuelve un listado con el código de pedido, código de cliente, 
+    --fecha esperada y fecha de entrega de los pedidos cuya fecha de entrega ha sido al menos dos días antes de la fecha esperada.
+    --Utilizando la función ADDDATE de MySQL.
+select codigo_pedido, codigo_cliente, fecha_esperada, fecha_entrega from pedido
+    where fecha_entrega <= adddate(fecha_esperada,-2);
+    --Utilizando la función SUBDATE de MySQL.
+select codigo_pedido, codigo_cliente, fecha_esperada, fecha_entrega from pedido --///--///--///--
+    where fecha_entrega <= subdate(fecha_esperada,2);
+    --Utilizando la función DATEDIFF de MySQL.
+select codigo_pedido, codigo_cliente, fecha_esperada, fecha_entrega from pedido
+    where datediff(fecha_esperada,fecha_entrega) >= 2;
+    --¿Sería posible resolver esta consulta utilizando el operador de suma + o resta -?
+select codigo_pedido,codigo_cliente,fecha_esperada,fecha_entrega from pedido
+    where  (fecha_esperada - fecha_entrega) >=2
+
+-- 11_Devuelve un listado de todos los pedidos que fueron rechazados en 2009.
+select * from pedido 
+    where estado = 'rechazado'  and year(fecha_pedido) ='2009'
+
+-- 12_Devuelve un listado de todos los pedidos que han sido entregados en el mes de enero de cualquier año.
+        -- select * from pedido 
+        --     where month(fecha_entrega) = 1 and fecha_entrega is not null;
+select *from pedido
+    where estado='entregado'  and month(fecha_pedido)='01'
