0d1395bcfa212ee545f377e0e67016051d763423
Osmel Torres <osmel86mtz@gmail.com>
2025-09-13 13:16:35 +0200

    Refactor queries to display clients and their sales representatives based on payment status in COMPOSICION_INTERNA.sql

--- SQL_playground/4_JARDINERIA/2_COMPOSICION_INTERNA/COMPOSICION_INTERNA.sql
+++ SQL_playground/4_JARDINERIA/2_COMPOSICION_INTERNA/COMPOSICION_INTERNA.sql
@@ -3,9 +3,16 @@ select nombre_cliente,nombre ,apellido1 from cliente c
     join empleado e where c.codigo_empleado_rep_ventas = e.codigo_empleado 
 
 -- 2_Muestra el nombre de los clientes que hayan realizado pagos junto con el nombre de sus representantes de ventas.
+-- IA
+select distinct nombre_cliente,nombre,apellido1 from cliente c
+    join empleado e on c.codigo_empleado_rep_ventas = e.codigo_empleado
+    join pago p on c.codigo_cliente = p.codigo_cliente
 
 -- 3_Muestra el nombre de los clientes que no hayan realizado pagos junto con el nombre de sus representantes de ventas.
-
+select  nombre_cliente,nombre,apellido1 from cliente c
+    join empleado e on c.codigo_empleado_rep_ventas = e.codigo_empleado
+    where c.codigo_cliente not in(select codigo_cliente from pago)
+    
 -- 4_
 
 -- 5_
