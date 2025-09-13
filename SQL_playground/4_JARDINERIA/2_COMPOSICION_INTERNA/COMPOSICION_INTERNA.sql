1961ddd4b00dba8d89261a114a5eb7da9e9924b1
Osmel Torres <osmel86mtz@gmail.com>
2025-09-13 12:15:05 +0200

    Add initial comments for client and sales representative queries in COMPOSICION_INTERNA.sql

---
+++ SQL_playground/4_JARDINERIA/2_COMPOSICION_INTERNA/COMPOSICION_INTERNA.sql
@@ -0,0 +1,11 @@
+-- 1_Obtén un listado con el nombre de cada cliente y el nombre y apellido de su representante de ventas.
+
+-- 2_Muestra el nombre de los clientes que hayan realizado pagos junto con el nombre de sus representantes de ventas.
+
+-- 3_Muestra el nombre de los clientes que no hayan realizado pagos junto con el nombre de sus representantes de ventas.
+
+-- 4_
+
+-- 5_
+
+-- 6_
