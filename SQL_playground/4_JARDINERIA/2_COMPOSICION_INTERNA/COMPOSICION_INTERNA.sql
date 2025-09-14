258e2949770ca1b16eaff3a4c97fb735e0346b4d
Osmel Torres <osmel86mtz@gmail.com>
2025-09-14 13:09:38 +0200

    Add query to retrieve clients who have made payments along with their representatives and office city in COMPOSICION_INTERNA.sql

--- SQL_playground/4_JARDINERIA/2_COMPOSICION_INTERNA/COMPOSICION_INTERNA.sql
+++ SQL_playground/4_JARDINERIA/2_COMPOSICION_INTERNA/COMPOSICION_INTERNA.sql
@@ -13,7 +13,11 @@ select  nombre_cliente,nombre,apellido1 from cliente c
     join empleado e on c.codigo_empleado_rep_ventas = e.codigo_empleado
     where c.codigo_cliente not in(select codigo_cliente from pago)
     
--- 4_
+-- 4_Devuelve el nombre de los clientes que han hecho pagos y el nombre de sus representantes junto con la ciudad de la oficina a la que pertenece el representante.
+select  nombre_cliente,nombre,apellido1,apellido2,o.ciudad from cliente c
+    join empleado e on c.codigo_empleado_rep_ventas = e.codigo_empleado
+    join oficina o on e.codigo_oficina = o.codigo_oficina
+    join pago p on c.codigo_cliente = p.codigo_cliente
 
 -- 5_
 
