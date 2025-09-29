d49b6a2afc288c4034babf4fbd2c6357a076b62d
Osmel Torres <osmel86mtz@gmail.com>
2025-09-29 23:40:22 +0200

    Añadir consultas para obtener los nombres de fabricantes con y sin productos asociados utilizando ALL o ANY.

--- SQL_playground/1_TIENDA/5_SUBCONSULTAS/SUBCONSULTAS.sql
+++ SQL_playground/1_TIENDA/5_SUBCONSULTAS/SUBCONSULTAS.sql
@@ -21,3 +21,21 @@ select * from producto p1
     where not exists
         (select 1 from producto p2 
             where p1.precio > p2.precio)   
+
+-- 9_Devuelve los nombres de los fabricantes que tienen productos asociados. (Utilizando ALL o ANY).
+select nombre from  fabricante f
+    where exists 
+        (select 1 from producto p
+            where p.id_fabricante = f.id)
+
+SELECT nombre
+    FROM fabricante f
+        WHERE f.id = ANY (
+            SELECT id_fabricante
+                FROM producto
+
+-- 10_Devuelve los nombres de los fabricantes que no tienen productos asociados. (Utilizando ALL o ANY).
+select nombre from fabricante f
+    where not exists
+        (select 1 from producto p
+            where f.id = p.id_fabricante )
